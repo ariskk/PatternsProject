32f1f3a4d41955c06c1f1b84b75e1aef26e0e837
aris <aris@100.66.199.46>
2014-02-15 17:20:22 +0000

    UserInterface design 2 and testMain

--- src/uk/ac/man/aris/ui/UserInterface.java
+++ src/uk/ac/man/aris/ui/UserInterface.java
@@ -7,6 +7,8 @@
 package uk.ac.man.aris.ui;
 
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.Toolkit;
 import java.util.HashSet;
 import javax.swing.JButton;
@@ -24,7 +26,7 @@ public class UserInterface extends JFrame {
    private final JPanel mainPanel;
    private final JButton submitButton;
    private final JTextField cardNumber;
-   private final JTextField firstName;
+   private final JTextField firstNameField;
    private final JLabel firstNameLabel;
    private final JLabel messageArea;
    
@@ -32,8 +34,8 @@ public class UserInterface extends JFrame {
    mainPanel=new JPanel();
    submitButton=new JButton();
    cardNumber=new JTextField();
-   firstName=new JTextField();
-   firstNameLabel=new JLabel("<b>First Name</b>");
+   firstNameField=new JTextField(30);
+   firstNameLabel=new JLabel("<html><b>First Name:</b></html>");
    messageArea=new JLabel("Welcome to UoM On-line Banking System");
    initComponents();
    
@@ -50,7 +52,30 @@ public class UserInterface extends JFrame {
          this.setTitle("Welcome to UoM Bank On-line Money Transfer System");
 
          /*Set-up Frame's Grid Layout   */
-         
+        mainPanel.setLayout(new GridBagLayout());
+        GridBagConstraints con = new GridBagConstraints();
+        
+        /* Add constraints and place GUI components   */
+        con.gridx=0;
+        con.gridy=0;
+        con.weightx=1.0;
+        con.weighty=1.0;
+        mainPanel.add(firstNameLabel,con);
+        con.gridx=1;
+        con.weightx=3.0;
+        mainPanel.add(firstNameField,con);
+        
+        con.gridx=0;
+        con.gridy=2;
+        con.weighty=4.0;
+        mainPanel.add(messageArea,con);
+        
+       
+        
+                
+        
+        
+        
          
          this.add(mainPanel);
          
