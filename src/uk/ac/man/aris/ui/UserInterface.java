885319366e1dd86458bbc2c38dbc1f23f0a62a75
mbaxkak4 <mbaxkak4@E-C07KI225B24.cs.man.ac.uk>
2014-02-21 18:05:32 +0000

    more gui

--- src/uk/ac/man/aris/ui/UserInterface.java
+++ src/uk/ac/man/aris/ui/UserInterface.java
@@ -4,6 +4,8 @@
  */
 package uk.ac.man.aris.ui;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
@@ -11,9 +13,20 @@ import javax.swing.JPanel;
  * @author mbaxkak4
  */
 public class UserInterface extends JPanel{
-
+    private final JLabel title;
+    private final JLabel convert;
+    private final JLabel submit;
+    private final JLabel toLabel;
+    private final JButton convertButton;
+    private final JButton submitButton;
     
     public UserInterface(){
+    title=new JLabel("");
+    convert=new JLabel("Convert");
+    submit=new JLabel("Submit");
+    toLabel=new JLabel("to");
+    convertButton=new JButton("Convert");
+    submitButton=new JButton("Submit");
     
     
     }
