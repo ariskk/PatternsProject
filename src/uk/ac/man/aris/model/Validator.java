bd28de830945c070b4980a998918c3d06eba5f8c
aris <aris@100.66.199.34>
2014-03-02 17:58:10 +0000

    -

---
+++ src/uk/ac/man/aris/model/Validator.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package uk.ac.man.aris.model;
+
+import uk.ac.man.aris.dao.Transaction;
+
+/**
+ *
+ * @author aris
+ */
+public class Validator {
+    
+    public Validator(){
+    
+    }
+    
+    public boolean validate(Transaction transaction){
+        
+        
+    return true;}
+}
